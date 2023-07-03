283b84ddf78adf813e2d8f1bae9e0ca6b456dd7c
Mihaela Chiculita <120111441+MihaelaChicu@users.noreply.github.com>
2023-07-03 19:06:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Book Class: Represents a Book
-class Book{
-    constructor(title, author, year){
+class Book {
+    constructor(title, author, year) {
         this.title = title;
         this.author = author;
         this.year = year;
     }
 }
 //UI Class: Handle UI Tasks
-class UI{
-    static displayBooks(){
-      const storedBooks = [
-        {
-          title:"Book One",
-          author:"John Doe",
-          year:"3434434"
-        },
-        {
-          title:"Book Two",
-          author:"Jane Doe",
-          year:"45545"
-        }
-      ];
+class UI {
+    static displayBooks() {
+        const storedBooks = [
+            {
+                title: "Book One",
+                author: "John Doe",
+                year: "3434434"
+            },
+            {
+                title: "Book Two",
+                author: "Jane Doe",
+                year: "45545"
+            }
+        ];
 
-      const books = storedBooks;
+        const books = storedBooks;
 
-      //loop through all the books in the array in the local storage 
-      //calling a method and passing a book to it
-      books.forEach((book) => UI.addBookToList(book));
+        //loop through all the books in the array in the local storage 
+        //calling a method and passing a book to it
+        books.forEach((book) => UI.addBookToList(book));
     }
 
-    static addBookToList(book){
-      //grab element
-      const list = document.getElementById('book-list');
-      
-      //create the table row to put into the tbody
-      const row = document.createElement('tr');
+    static addBookToList(book) {
+        //grab element
+        const list = document.getElementById('book-list');
 
-      row.innerHTML = `
+        //create the table row to put into the tbody
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.year}</td>
@@ -44,15 +44,41 @@ class UI{
     `;
 
 
-      //append row/book to the list
-    list.appendChild(row);
+        //append row/book to the list
+        list.appendChild(row);
     }
 
+    static deleteBook(element) {
+        if (element.classList.contains('delete')) {
+
+            //delete parent element - the whole row
+            element.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className){
+      const div = document.createElement('div');
+
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+
+      //to add the element to the DOM, we need to get elements before & after
+      //before - container
+      const container = document.querySelector('.container');
+
+      //after - form
+      const form = document.querySelector('#book-form');
+
+      //insert the alert by taking the container(parent) and insert the div before the form
+      container.insertBefore(div, form);
+    }
+
+
     static clearFields() {
-      // Grab each value and clear it
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
-      document.querySelector('#year').value = '';
+        // Grab each value and clear it
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#year').value = '';
 
     }
 }
@@ -72,13 +98,25 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const year = document.querySelector('#year').value;
 
-    // Instantiate a new book from the Book class to add a book
-    const book = new Book(title, author, year);
-   
-    // Add book to UI
-    UI.addBookToList(book);
+    //Make sure all fields are filled in before submitting 
+    if (title === '' || author === '' || year === '') {
+        alert('Please fill in all fields');
+    } else {
+        // Instantiate a new book from the Book class to add a book
+        const book = new Book(title, author, year);
 
-   // Clear field method
-   UI.clearFields();
+        // Add book to UI
+        UI.addBookToList(book);
+
+        // Clear field method
+        UI.clearFields();
+    }
+
+
 });
+
 //Event: Remove a book (UI/Local Storage)
+//Event Propagation: delete the actual list
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+})
